09808dad4cc54dfe8c7bd870c5d5d0b557a701e7
Michael <aquausdev@gmail.com>
2016-11-20 02:37:08 +0000

    Add files via upload

---
+++ game.py
@@ -0,0 +1,12 @@
+class VariableHandling():
+    """Runs the init and mangement of variables"""
+
+class GeneralFunctions():
+    """Runs the functions that control the game"""
+
+class GameManagement():
+    """Runs the management of the game"""
+
+class DataHandling():
+    """Runs functions such as saving, loading and qutting"""
+    
